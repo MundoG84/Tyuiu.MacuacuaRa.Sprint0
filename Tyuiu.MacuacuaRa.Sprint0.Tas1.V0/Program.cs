32fd515f752c721cff5f968f730ce0581ba9310f
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-27 23:19:22 +0500

    Task1

---
+++ Tyuiu.MacuacuaRa.Sprint0.Tas1.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.MacuacuaRa.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "MacuacauRa";
+            int age = 52;
+
+            Console.WriteLine("Здравствуйте  меня зовут " + name + " мне " + age + " лет ");
+            Console.ReadKey();
+        }
+    }
+}
