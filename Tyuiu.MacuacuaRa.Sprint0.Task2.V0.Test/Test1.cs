ec55da825226e856177ba6315a345f27dfbfc7f8
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-27 23:21:46 +0500

    Task2

---
+++ Tyuiu.MacuacuaRa.Sprint0.Task2.V0.Test/Test1.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MacuacuaRa.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MacuacuaRa.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var name = "Macuacua";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("привет, Macuacua", res);
+        }
+    }
+}
---
+++ Tyuiu.MacuacuaRa.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.MacuacuaRa.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MacuacuaRa.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Macuacua"));
+            Console.ReadKey();
+        }
+    }
+}
