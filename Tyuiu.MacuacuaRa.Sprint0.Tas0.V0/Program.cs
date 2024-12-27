78acf2628e618b0cae1107f9da05c01da1b914c5
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-27 23:17:30 +0500

    Add project files.

---
+++ Tyuiu.MacuacuaRa.Sprint0.Tas0.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.MacuacuaRa.Sprint0.Tas0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
